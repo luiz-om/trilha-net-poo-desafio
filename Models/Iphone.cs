fd2a3c3c5488cc42c61c1aacc2a2fb924d55d91a
LuizOM <luizfernando_martins@hotmail.com>
2023-09-26 00:18:42 -0400

    envio

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public int MyProperty { get; set; }
+        public Iphone(string numero, string modelo, string imei, int memoria, int myProperty) : base(numero, modelo, imei, memoria)
+        {
+MyProperty = myProperty;
+        }
+
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+           Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone");
+           
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
-Console.WriteLine($"Text");
+
+
+var nokia = new Nokia("123","mp34","222555", 64);
+var iphone = new Iphone("5588","11 pro","111555", 128,1);
+
+nokia.Ligar();
+
+nokia.InstalarAplicativo("telegram");
+System.Console.WriteLine(iphone.MyProperty);
